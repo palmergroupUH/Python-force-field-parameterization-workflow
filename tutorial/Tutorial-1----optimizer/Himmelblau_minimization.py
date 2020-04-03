760a8fbc7e89d35a8799055e8761156c6a3d9194
Jingxiang Guo <jguo10@uh.edu>
2020-04-03 00:07:08 -0500

    updated tutorial with current optimizer

---
+++ tutorial/Tutorial-1----optimizer/Himmelblau_minimization.py
@@ -0,0 +1,20 @@
+import optimizer.gradient_free
+import objective.test_optimizer
+
+
+
+x = [-8,8,0.15]  # start, end, step
+
+y =[-8,8,0.15] # start, end, step
+
+Himmelblau_test_obj = objective.test_optimizer.Himmelblau(x,y)  
+
+#Himmelblau_test.visualize()
+
+optimize_Himmelblau = optimizer.gradient_free.NelderMeadSimplex("in_himmelblau","himmelblau.log",Himmelblau_test_obj,skipped=0)
+
+optimize_Himmelblau.run_optimization()
+
+ 
+
+
