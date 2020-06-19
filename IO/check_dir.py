22d467d3d000548dab70630739b6e4cfbd7f5f45
Jingxiang Guo <jguo10@uh.edu>
2020-06-19 11:01:59 -0500

    make check_dir.py PEP8 compilant

--- IO/check_dir.py
+++ IO/check_dir.py
@@ -1,79 +1,87 @@
-import os 
-import sys 
-import time 
+# Python standard library:
+import numpy as np
+import os
+import sys
+import time
 import logging
 
-def mkdirs_if_not_exist(folder_path,overwrite):  
+# Local library:
+
+# Third-party libraries:
+
+
+def mkdirs_if_not_exist(folder_path, overwrite):
 
     folder_create = logging.getLogger(__name__)
 
-    if ( not os.path.isdir(folder_path)): 
+    if (not os.path.isdir(folder_path)):
 
-        os.makedirs(folder_path) 
+        os.makedirs(folder_path)
 
-        return None  
-   
-    if ( overwrite ):  
+        return None
 
-        os.system("rm -r %s"%folder_path)  
-    
-        time.sleep(0.5) 
-    
-        os.makedirs(folder_path) 
-   
-    elif ( not overwrite and os.path.isdir(folder_path)):   
+    if (overwrite):
 
-        folder_create.error("The folder: '%s' already exists ! It can not be overwritten; please set 'overwrite=True' "%folder_path)
-        
+        os.system("rm -r %s" % folder_path)
+
+        time.sleep(0.5)
+
+        os.makedirs(folder_path)
+
+    elif (not overwrite and os.path.isdir(folder_path)):
+
+        folder_create.error("The folder: '%s' already exists ! "
+                            "It can not be overwritten; "
+                            "please set 'overwrite=True' " % folder_path)
+
         sys.exit("check errors in log file")
 
-    return None 
-    
-def check_folder_status(folder_path):  
+    return None
 
-    exist = True 
 
-    empty = False 
+def check_folder_status(folder_path):
 
-    if ( not os.path.isdir(folder_path)): 
+    exist = True
+
+    empty = False
+
+    if (not os.path.isdir(folder_path)):
 
         exist = False
 
-        return exist,empty
+        return exist, empty
 
-    content = os.listdir(folder_path) 
+    content = os.listdir(folder_path)
 
-    if ( len(content ) == 0 ):
+    if (len(content) == 0):
 
-        empty = True 
+        empty = True
 
-    return exist,empty 
+    return exist, empty
 
-def decide_folder_status(folder_path,mesg=None):
-    
-    if ( mesg is None): 
+
+def decide_folder_status(folder_path, mesg=None):
+
+    if (mesg is None):
 
         mesg = ""
 
-    exist,empty = check_folder_status(folder_path)
+    exist, empty = check_folder_status(folder_path)
 
-    folder_status = logging.getLogger(__name__)  
+    folder_status = logging.getLogger(__name__)
 
-    if ( not exist ): 
-         
-        folder_status.error("ERROR: %s Folder: %s does not exist "%(mesg,folder_path))
+    if (not exist):
 
-        sys.exit("Check errors in log file !") 
+        folder_status.error("ERROR: %s Folder: %s does not exist "
+                            % (mesg, folder_path))
 
-    if ( empty ): 
+        sys.exit("Check errors in log file !")
 
-        folder_status.error("ERROR: %s Folder: %s  is empty !  "%(mesg,folder_path))
+    if (empty):
 
-        sys.exit("Check errors in log file !") 
-       
-    return None  
-        
-        
-        
-        
+        folder_status.error("ERROR: %s Folder: %s  is empty !"
+                            % (mesg, folder_path))
 
+        sys.exit("Check errors in log file !")
+
+    return None
